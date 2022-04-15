ac5b52a60ecd652eeb0e4c111f2af42c613f7894
Julien coeurvolan <jcvolan@gmail.com>
2022-04-15 17:35:06 +0200

    fix react 18 warning

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import './index.scss';
 import {Main} from './components/main/main';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Main/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(<Main/>);
+}
 
